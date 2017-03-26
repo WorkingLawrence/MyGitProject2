044b506db34adfb3fb25e981f69beff85880b4b5
lawrencewithandroid <tobefree2233@gmail.com>
2017-03-27 01:10:34 +0800

    Config Commit

--- app/src/main/java/app/law/testgitprj2/MainActivity.java
+++ app/src/main/java/app/law/testgitprj2/MainActivity.java
@@ -1,9 +1,8 @@
 package app.law.testgitprj2;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.ListViewCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
