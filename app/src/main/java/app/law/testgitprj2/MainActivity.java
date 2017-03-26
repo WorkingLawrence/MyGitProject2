55fc559c1dea4187536c9164b7414f3c4d842ea9
lawrencewithandroid <tobefree2233@gmail.com>
2017-03-27 00:55:55 +0800

    More Commit

--- app/src/main/java/app/law/testgitprj2/MainActivity.java
+++ app/src/main/java/app/law/testgitprj2/MainActivity.java
@@ -3,8 +3,10 @@ package app.law.testgitprj2;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ListViewCompat;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
@@ -22,6 +24,7 @@ public class MainActivity extends AppCompatActivity
     {
         button1 = (Button) findViewById(R.id.button);
         button2 = (Button) findViewById(R.id.button2);
+        mainlistview = (ListView) findViewById(R.id.mainlistview);
         initListeners();
 
     }
@@ -50,4 +53,5 @@ public class MainActivity extends AppCompatActivity
 
 
     private Button button1, button2;
+    private ListView mainlistview;
 }
