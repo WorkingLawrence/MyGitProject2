5c82caf98b13bafd1aab02c132b491cfa8a4d1f2
lawrencewithandroid <tobefree2233@gmail.com>
2017-03-27 01:46:03 +0800

    Person Fields Change

--- app/src/main/java/app/law/testgitprj2/dto/TPerson.java
+++ app/src/main/java/app/law/testgitprj2/dto/TPerson.java
@@ -6,6 +6,39 @@ import java.io.Serializable;
 public class TPerson implements Serializable
 {
     private String personId;
+    private String personName;
+    private String personAddress;
+    private String personPhoneNum;
+
+    public String getPersonName()
+    {
+        return personName;
+    }
+
+    public void setPersonName(String personName)
+    {
+        this.personName = personName;
+    }
+
+    public String getPersonAddress()
+    {
+        return personAddress;
+    }
+
+    public void setPersonAddress(String personAddress)
+    {
+        this.personAddress = personAddress;
+    }
+
+    public String getPersonPhoneNum()
+    {
+        return personPhoneNum;
+    }
+
+    public void setPersonPhoneNum(String personPhoneNum)
+    {
+        this.personPhoneNum = personPhoneNum;
+    }
 
     public String getPersonId()
     {
